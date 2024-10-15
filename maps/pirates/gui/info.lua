3b54e2c8296f9bda26f0a05c98fe72f08b8171ec
danielmartin0 <github@danielgmartin.com>
2024-10-15 22:59:50 +0100

    fix info

--- maps/pirates/gui/info.lua
+++ maps/pirates/gui/info.lua
@@ -244,7 +244,7 @@ function Public.full_update(player)
 	elseif flow2.selected_tab_index == 2 then
 		flow2.style.height = 500
 	elseif flow2.selected_tab_index == 3 then
-		flow2.style.height = 800
+		flow2.style.height = 500
 	elseif flow2.selected_tab_index == 4 then
 		flow2.style.height = 350
 	end
