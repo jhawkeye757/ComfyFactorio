37fccb4e1647b87d400503664b47513f7bf72239
jhawkeye757 <jhawkeye757@gmail.com>
2024-12-08 17:55:53 -0500

    Update quality_global.lua
    apply to new surfaces

--- modules/quality_global.lua
+++ modules/quality_global.lua
@@ -3,13 +3,23 @@ local Event = require 'utils.event'
 -- Function to set global effects on all surfaces
 local function set_global_effect()
     for _, surface in pairs(game.surfaces) do
-        surface.global_effect = {quality = 0.1}
+        surface.global_effect = {quality = 0.2}
     end
 
     -- Unregister the on_tick event after execution
     script.on_event(defines.events.on_tick, nil)
 end
 
+-- Function to handle newly created surfaces
+local function on_surface_created(event)
+    local surface = game.surfaces[event.surface_index]
+    if surface then
+        surface.global_effect = {quality = 0.2}
+    end
+end
+
+-- Register the function for relevant events
 return {
-    Event.on_init(set_global_effect)
+    Event.on_init(set_global_effect),
+    Event.add(defines.events.on_surface_created, on_surface_created)
 }
